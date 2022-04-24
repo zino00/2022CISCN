36999eb290afd97fb469cf6efd11e387c3a65ac5
sun-ruijiang <3151425690@qq.com>
2022-04-24 14:49:03 +0800

    get linenumber of single c file

--- slice/get_sensitive_funcname.py
+++ slice/get_sensitive_funcname.py
@@ -1,19 +1,12 @@
-'''
-Author: Sung
-Date: 2022-04-24 11:59
-LastEditors: Sung
-LastEditTime: 2022-04-24 12:04
-FilePath: \2022CISCN\slice\get_sensitive_funcname.py
-Description: 通过反序列化 sensitive_func.pkl 得到 sensitive_funcname.txt 敏感函数名
-Copyright (c) 2022 by Sung, All Rights Reserved. 
-'''
 import pickle
 
+# pkl 得到 txt 文件
 
-def pkl2txt(pkl_path, sc_path):
+
+def pkl2txt(pkl_path, txt_path):
     with open(pkl_path, 'rb') as fin:
         list_sensitive_funcname = pickle.load(fin)
-    with open(sc_path, 'w') as txt_file:
+    with open(txt_path, 'w') as txt_file:
         for func_name in list_sensitive_funcname:
             txt_file.write(func_name + '\n')
 
--- slice/get_vul_linenumber.py
+++ slice/get_vul_linenumber.py
@@ -1,12 +1,3 @@
-'''
-Author: Sung
-Date: 2022-04-24 11:58
-LastEditors: Sung
-LastEditTime: 2022-04-24 12:05
-FilePath: \2022CISCN\slice\get_vul_linenumber.py
-Description: 得到可能的漏洞行号
-Copyright (c) 2022 by Sung, All Rights Reserved. 
-'''
 from operator import itemgetter
 import subprocess
 import os
@@ -14,48 +5,23 @@ import time
 import tqdm
 import json
 import re
-# C file folder path
+'''
+description: 该文件需要设置 C_source_dir、SC_path、sensitive_funcname_txt_path
+运行命令: python3 ./get_vul_linenumber
+'''
+
+# c 源文件目录
 C_source_dir = "/home/sung/Desktop/joern/Juliet_Test_Suite_v1.3_for_C_Cpp/C/testcases/"
-# .sc script path
+# Joern 运行的 .sc 脚本
 SC_path = '/home/sung/Desktop/joern/script/cpg_extract.sc'
-# cpg.bin file path
-CPG_source_path = './data/parsed/cpg/'
-# all properties json file path
-Json_source_path = './data/parsed/json/'
-# result json path
-Res_source_path = './data/parsed/res/'
-# sensittive function name txt file path
+# txt 文件路径 包含敏感函数名
 sensitive_funcname_txt_path = "/home/sung/Desktop/joern/script/sensitive_funcname.txt"
 
-
-def c2cpg(c_File_Path, cpg_Path):
-    # parse source code into cpg
-    print('parsing source code into cpg...')
-    for filepath, dirnames, filenames in os.walk(c_File_Path):
-        for item in filenames:
-            print(item)
-            if item[-2:] == '.c':
-                print("start")
-                filename = item[:-2] + '.bin'
-                shell_command = "joern-parse --output " + filename + " " + c_File_Path + item
-                subprocess.call(shell_command, shell=True)
-                # move cpg into cpg dir
-                shell_command = "mv -b ./" + filename + " " + cpg_Path
-                subprocess.call(shell_command, shell=True)
-
-
-def cpg2json(cpg_folder_Path, json_folder_Path, sc_path=SC_path):
-    print('json')
-    for filepath, dirnames, filenames in os.walk(cpg_folder_Path):
-        for item in filenames:
-            print(item)
-            filename = item[:-4] + '.json'
-            cpg_Path = cpg_folder_Path + '/' + item
-            project_Path = item[:-4] + ".bin"
-            shell_command = "joern --script " + sc_path + " --params cpgFile=" + \
-                cpg_Path + ",projectFolder=" + project_Path + \
-                ",outFile=" + json_folder_Path + filename
-            subprocess.call(shell_command, shell=True)
+'''
+description: 将 txt 文件里面的内容提取到一个 function_list 列表
+param {string} txt_path: txt 文件路径
+return {list} function_list: 返回 function_list 列表
+'''
 
 
 def get_all_function_list(txt_path):
@@ -66,84 +32,99 @@ def get_all_function_list(txt_path):
     return function_list
 
 
-def extract_properties(json_folder_path, result_folder_path, function_list):
-    print('result')
-    for filepath, dirnames, filenames in os.walk(json_folder_path):
+'''
+description: 使用joern-parse解析一个c文件得到cpg图,对cpg图进行josrn分析,提取可能的漏洞代码和行号,保存在一个json文件
+param {string} c_Filename
+param {list} function_list
+'''
+
+
+def c2res(c_Filename, function_list):
+    for filepath, dirnames, filenames in os.walk(C_source_dir):
+        flag = False
+        for item in filenames:
+            if c_Filename == item:
+                c_file_path = filepath + '/' + item
+                c_file_dir = filepath
+                # parse source code into cpg
+                print('parsing source code into cpg...')
+                print(c_file_path + "\n")
+                cpg_filename = c_Filename[:-2] + '.bin'
+                shell_command = "joern-parse --output " + cpg_filename + " " + c_file_path
+                subprocess.call(shell_command, shell=True)
+                # move cpg into .c file dir
+                shell_command = "mv -b ./" + cpg_filename + " " + c_file_dir
+                subprocess.call(shell_command, shell=True)
+                # cpg into json
+                json_filename = c_Filename[:-2] + '.json'
+                project_filename = c_Filename[:-2] + ".bin"
+                json_file_path = c_file_dir + '/' + json_filename
+                cpg_file_path = c_file_dir + '/' + cpg_filename
+                shell_command = "joern --script " + SC_path + " --params cpgFile=" + \
+                    cpg_file_path + ",projectFolder=" + os.path.abspath(project_filename) + \
+                    ",outFile=" + json_file_path
+                subprocess.call(shell_command, shell=True)
+                # json into result
+                result_filename = c_Filename[:-2] + '_res.json'
+                result_file_path = c_file_dir + '/' + result_filename
+                with open(json_file_path) as file:
+                    properties_data = json.load(file)
+                with open(result_file_path, 'w') as result:
+                    allres = []
+                    variable_list = []
+                    for item in properties_data:
+                        if item['_label'] == "LOCAL":
+                            variable_list.append(item['name'])
+                    for item in properties_data:
+                        if '*' in item['code'] or '[' in item['code']:
+                            res = {}
+                            res['name'] = item['name']
+                            res['lineNumber'] = item['lineNumber']
+                            allres.append(res)
+                            print(res)
+                        elif '<operator>.assignment' == item['name'] and variable_list.count(re.split('=|\+|\-', item['code'])[1]) != 0:
+                            res = {}
+                            res['code'] = re.split('=', item['code'])[0]
+                            res['lineNumber'] = item['lineNumber']
+                            allres.append(res)
+                            print(res)
+                        elif function_list.count(item['name']) != 0:
+                            res = {}
+                            res['name'] = item['name']
+                            res['lineNumber'] = item['lineNumber']
+                            allres.append(res)
+                            print(res)
+                    print(allres)
+                    json.dump(allres, result)
+                    flag = True
+                    break
+        if flag == True:
+            break
+
+
+'''
+description: 对c_folder_dir目录下的所有c文件进行分析
+param {string} c_folder_dir: 要分析的c源代码目录
+param {string} functino_list: 敏感函数列表
+'''
+
+
+def test_c2res(c_folder_dir, functino_list):
+    for filepath, dirnames, filenames in os.walk(c_folder_dir):
         for item in filenames:
             print(item)
-            with open(json_folder_path + item) as file:
-                properties_data = json.load(file)
-            with open(result_folder_path + item, 'w') as result:
-                allres = []
-                variable_list = []
-                for item in properties_data:
-                    if item['_label'] == "LOCAL":
-                        variable_list.append(item['name'])
-                for item in properties_data:
-                    if '*' in item['code'] or '[' in item['code']:
-                        res = {}
-                        res['name'] = item['name']
-                        res['lineNumber'] = item['lineNumber']
-                        allres.append(res)
-                        print(res)
-                    elif '<operator>.assignment' == item['name'] and variable_list.count(re.split('=|\+|\-', item['code'])[1]) != 0:
-                        res = {}
-                        res['code'] = re.split('=', item['code'])[0]
-                        res['lineNumber'] = item['lineNumber']
-                        allres.append(res)
-                        print(res)
-                    elif function_list.count(item['name']) != 0:
-                        res = {}
-                        res['name'] = item['name']
-                        res['lineNumber'] = item['lineNumber']
-                        allres.append(res)
-                        print(res)
-                print(allres)
-                json.dump(allres, result)
-
-
-def all_c2cpg():
-    dirs = os.listdir(C_source_dir)
-    for c_folder in tqdm.tqdm(dirs):
-        c_file_path = C_source_dir + c_folder
-        cpg_path = CPG_source_path + c_folder
-        os.makedirs(cpg_path)
-        print(f'starting to parse {c_file_path}')
-        c2cpg(c_file_path, cpg_path)
-
-
-def all_cpg2json():
-    dirs = os.listdir(CPG_source_path)
-    for cpg_folder in tqdm.tqdm(dirs):
-        cpg_folder_path = CPG_source_path + cpg_folder
-        json_path = Json_source_path + cpg_folder + '/'
-        os.makedirs(json_path)
-        cpg2json(cpg_folder_path, json_path)
-
-
-def all_extract_properties():
-    dirs = os.listdir(Json_source_path)
-    function_list = get_all_function_list(sensitive_funcname_txt_path)
-    for json_folder in tqdm.tqdm(dirs):
-        json_folder_path = Json_source_path + json_folder
-        result_folder_path = Res_source_path + json_folder + '/'
-        os.makedirs(result_folder_path)
-        extract_properties(json_folder_path, result_folder_path, function_list)
+            if item[-2:] == '.c':
+                c2res(item, functino_list)
 
 
 if __name__ == "__main__":
     start = time.time()
+    
+    # 得到敏感函数列表 function_list
     function_list = get_all_function_list(sensitive_funcname_txt_path)
-    c2cpg(C_source_dir + "CWE15_External_Control_of_System_or_Configuration_Setting/",
-          CPG_source_path)
-    cpg2json(CPG_source_path,
-             Json_source_path,
-             SC_path)
-    extract_properties(Json_source_path,
-                       Res_source_path,
-                       function_list)
-    # all_c2cpg()
-    # all_cpg2json()
-    # all_extract_properties()
+    # 分析 C_source_dir + "CWE15_External_Control_of_System_or_Configuration_Setting/" 目录下面的c文件
+    test_c2res(C_source_dir + "CWE15_External_Control_of_System_or_Configuration_Setting/", function_list)
+    
+    # 计算程序运行时间
     end = time.time()
     print("循环运行时间:%.2f秒" % (end-start))
